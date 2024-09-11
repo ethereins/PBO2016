4c3b4600059eecae7692cf388c9c182a2fe7b43b
ethereins <indahsyahfitri084@gmail.com>
2024-09-11 09:49:53 +0700

    commit - laundry pekan 1

---
+++ laundryapps/src/model/Customer.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Customer {
+	String id, nama, alamat, NoHp;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getAlamat() {
+		return alamat;
+	}
+	public void setAlamat(String alamat) {
+		this.alamat = alamat;
+	}
+	public String getNoHp() {
+		return NoHp;
+	}
+	public void setNoHp(String noHp) {
+		NoHp = noHp;
+	}
+	
+	
+}
