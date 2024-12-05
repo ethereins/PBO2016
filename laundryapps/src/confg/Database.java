43ca62aa0508c3fcabc62b723f610d2b9d486292
realme <realme@LAPTOP-MPCVDPEG>
2024-12-05 19:06:36 +0700

    update

--- laundryapps/src/confg/Database.java
+++ laundryapps/src/confg/Database.java
@@ -3,23 +3,23 @@ package confg;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.swing.JOptionPane;
-
+ 
 public class Database {
-    private static Connection koneksi;
+    private static Connection connection;
 
+    private static final String URL = "jdbc:mysql://localhost/laundry_apps";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    // Singleton getConnection
     public static Connection getConnection() {
-        if (koneksi == null) {
+        if (connection == null) {
             try {
-                Class.forName("com.mysql.cj.jdbc.Driver");  
-                koneksi = DriverManager.getConnection("jdbc:mysql://localhost:3306/laundry_apps?useSSL=false&serverTimezone=UTC", "root", "");
-                System.out.println("Koneksi berhasil!");
-            } catch (ClassNotFoundException e) {
-                JOptionPane.showMessageDialog(null, "Driver tidak ditemukan: " + e.getMessage());
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
             } catch (SQLException e) {
-                JOptionPane.showMessageDialog(null, "Koneksi gagal: " + e.getMessage());
+                System.err.println("Koneksi ke database gagal: " + e.getMessage());
             }
         }
-        return koneksi;
+        return connection;
     }
 }
